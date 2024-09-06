f24e94afa8ed14c027ddad9b713626afa4f37f76
Keaton Hall <116615707+MrK700@users.noreply.github.com>
2024-09-06 11:42:34 -0600

    Added more dad jokes (for real this time)

--- app.py
+++ app.py
@@ -101,6 +101,10 @@ def dad_joke():
         "What do you call fake spaghetti? An impasta!",
         "Why did the scarecrow win an award? Because he was outstanding in his field!",
         "I would avoid the sushi if I was you. It’s a little fishy."
+        "Today, my son asked 'Can I have a book mark?' and I burst into tears. 11 years old and he still doesn't know my name is Brian. "
+        "I went to the aquarium this weekend, but I didn’t stay long. There’s something fishy about that place."
+        "I gave my handyman a to-do list, but he only did jobs 1, 3, and 5. Turns out he only does odd jobs."
+        "I’m reading a horror story in braille. Something bad is going to happen, I can just feel it."
     ]
     joke = random.choice(jokes)
     return jsonify({"joke": joke})
