ac6b36d6b7df9ce62afc3fb960ecb6b3ae821532
SorenBybee <d00462732@utahtech.edu>
2024-09-11 15:25:17 -0600

    added tests for dad joke endpoint and moved dadjoke endpoint code inside the create_app function.

--- app.py
+++ app.py
@@ -10,6 +10,23 @@ def create_app():
     def hello_world():
         return "Hello World"
 
+        
+    @app.route('/dadjoke', methods=['GET'])
+    def dad_joke():
+        jokes = [
+            "Why don't skeletons fight each other? They don't have the guts.",
+            "What do you call fake spaghetti? An impasta!",
+            "Why did the scarecrow win an award? Because he was outstanding in his field!",
+            "I would avoid the sushi if I was you. It’s a little fishy.",
+            "Today, my son asked 'Can I have a bookmark?' and I burst into tears. 11 years old and he still doesn't know my name is Brian.",
+            "I went to the aquarium this weekend, but I didn’t stay long. There’s something fishy about that place.",
+            "I gave my handyman a to-do list, but he only did jobs 1, 3, and 5. Turns out he only does odd jobs.",
+            "I’m reading a horror story in braille. Something bad is going to happen, I can just feel it."
+        ]
+        joke = random.choice(jokes)
+        return jsonify({"joke": joke})
+
+
     return app
 
 app = create_app()
@@ -112,21 +129,6 @@ def pizza_toppings():
         "toppings": selected_toppings
     }
     return jsonify(pizza)
-
-@app.route('/dadjoke', methods=['GET'])
-def dad_joke():
-    jokes = [
-        "Why don't skeletons fight each other? They don't have the guts.",
-        "What do you call fake spaghetti? An impasta!",
-        "Why did the scarecrow win an award? Because he was outstanding in his field!",
-        "I would avoid the sushi if I was you. It’s a little fishy."
-        "Today, my son asked 'Can I have a book mark?' and I burst into tears. 11 years old and he still doesn't know my name is Brian. "
-        "I went to the aquarium this weekend, but I didn’t stay long. There’s something fishy about that place."
-        "I gave my handyman a to-do list, but he only did jobs 1, 3, and 5. Turns out he only does odd jobs."
-        "I’m reading a horror story in braille. Something bad is going to happen, I can just feel it."
-    ]
-    joke = random.choice(jokes)
-    return jsonify({"joke": joke})
 
 @app.route('/travel', methods=['GET','POST'])
 def travel():
