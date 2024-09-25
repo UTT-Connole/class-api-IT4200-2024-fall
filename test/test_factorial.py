7b1e17c81249fc893747a94e273725cd9aa6b81e
jacevtb0419 <d00434672@utahtech.edu>
2024-09-25 09:50:24 -0600

    finally fixed factorial tests

--- test/test_factorial.py
+++ test/test_factorial.py
@@ -1,4 +1,23 @@
-
+# test for 0
 def test_zero(client):
-    response = client.get('/factorial')
+    response = client.get('/factorial?n=0')
     assert response.status_code == 200
+  
+# test for 1
+def test_one(client):
+    response = client.get('/factorial?n=1')
+    assert response.status_code == 200
+
+# test a small number
+def test_five(client):
+    response = client.get('/factorial?n=5')
+    assert response.status_code == 200
+
+# test a big number
+def test_ten(client):
+    response = client.get('/factorial?n=10')
+    assert response.status_code == 200
+
+def test_negative(client):
+    response = client.get('/factorial?n=-1')
+    assert response.status_code == 400
