a6c6330d27f772d9f4af4167d4c1d6d38cbddd8a
Brayden Connole <brayden.connole@gmail.com>
2024-09-25 11:27:19 -0600

    remove broken tests

--- test/test_factorial.py
+++ test/test_factorial.py
@@ -2,9 +2,3 @@
 def test_zero(client):
     response = client.get('/factorial')
     assert response.status_code == 200
-
-def test_fail(client):
-    assert False
-
-def test_True(client):
-    assert True
