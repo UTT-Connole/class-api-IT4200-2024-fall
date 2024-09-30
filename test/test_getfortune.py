b4a678203f63f1e6687ff6624bbe84e0681b7f06
Yomi <aodubiyi1@gmail.com>
2024-09-30 11:04:48 -0600

    basketball facts all done

--- test/test_getfortune.py
+++ test/test_getfortune.py
@@ -1,16 +1,17 @@
 
-#def test_fortune_status_code(client: Any):
-    #"""Test if the fortune endpoint returns a 200 status code"""
-    #response = client.get('/fortune')
-    #assert response.status_code == 200
+def test_fortune_status_code(client):
+    """Test if the fortune endpoint returns a 200 status code"""
+    response = client.get('/fortune')
+    assert response.status_code == 200
 
-#def test_fortune_structure(client: Any):
-    #"""Test if the response structure is a JSON object with 'fortune' key"""
-    #response = client.get('/fortune')
-    #json_data = response.get_json()
-    #assert 'fortune' in json_data 
-#def test_fortune_not_empty(client: Any):
-   # """Test if the returned fortune is not an empty string"""
-    #response = client.get('/fortune')
-    #json_data = response.get_json()
-    #assert json_data['fortune'] != ''
+def test_fortune_structure(client):
+    """Test if the response structure is a JSON object with 'fortune' key"""
+    response = client.get('/fortune')
+    json_data = response.get_json()
+    assert 'fortune' in json_data
+
+def test_fortune_not_empty(client):
+    """Test if the returned fortune is not an empty string"""
+    response = client.get('/fortune')
+    json_data = response.get_json()
+    assert json_data['fortune'] != ''
