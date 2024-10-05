aacfe60a4ee1c5d2e801804c8e1c3e18f86335fa
Andres <143202987+d00411174@users.noreply.github.com>
2024-10-05 14:43:32 -0600

    removed old endpoint in app.py (#151)

--- endpoints/pizza.py
+++ endpoints/pizza.py
@@ -6,24 +6,30 @@ pizza_bp = Blueprint('pizza', __name__)
 
 @pizza_bp.route('/pizzaToppings', methods=['GET'])
 def pizza_toppings():
+    sizes = ["Small", "Medium", "Large"]  # Available pizza sizes
     sauces = ["Tomato Sauce", "Alfredo Sauce", "Ranch Sauce"]
     toppings = [
-        "Pepperoni", "Mushrooms", "Sausage", "Bacon",
-        "Extra cheese", "Pineapple", "Spinach"
+        {"topping": "Pepperoni"},
+        {"topping": "Mushrooms"},
+        {"topping": "Sausage"},
+        {"topping": "Bacon"},
+        {"topping": "Extra cheese"},
+        {"topping": "Pineapple"},
+        {"topping": "Spinach"}
     ]
     crusts = ["Hand Tossed", "Handmade Pan", "Crunchy Thin Crust"]
 
-    # Randomly choose one sauce, three toppings, and one crust
-    selected_sauce = random.choice(sauces)
-    selected_toppings = random.sample(toppings, 3)
+    selected_size = random.choice(sizes)  # Select a size at random
     selected_crust = random.choice(crusts)
+    selected_sauce = random.choice(sauces)  
+    selected_toppings = random.sample(toppings, 3)  # This should correctly choose dictionaries
 
-    # Prepare the pizza data
+    # Constructing the pizza dictionary with 'size' at the top
     pizza = {
+        "size": selected_size,  
         "crust": selected_crust,
         "sauce": selected_sauce,
         "toppings": selected_toppings
     }
 
-    # Return the pizza data as JSON
     return jsonify(pizza)
