de86c37c95400d74cc90b0bb9712c4641714279a
Laura Coulome <laura@Lauras-MacBook-Air.local>
2024-10-11 22:47:30 -0600

    added functionality to howtomakeendpoint and randomfacts

--- test/test_howtomakeendpoint.py
+++ test/test_howtomakeendpoint.py
@@ -2,8 +2,13 @@ import pytest
 from app import create_app
 
 def test_content_format(client):
-    """Test to see if howtomakeendpoint returns JSON format"""
+    """Test to see if howToMakeEndpoint returns JSON format"""
     response = client.get("/howToMakeEndpoint")
     assert response.content_type == 'application/json'
 
+def test_get_specific_step(client):
+    """Test to retrieve a specific step using the 'step' query parameter"""
+    response = client.get("/howToMakeEndpoint?step=2")
+    assert response.status_code == 200
+    assert response.json == {"step 2": "Create app"}
 
