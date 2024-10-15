ccccbc5a0b5ed16a27147f95fcb984ae7ce8f59a
JoshBeckstrand <98860088+JoshBeckstrand@users.noreply.github.com>
2024-10-15 10:07:36 -0600

    week6 test (#169)
    * week6 test
    
    * item test updat
    
    * fixing tests
    
    ---------
    
    Co-authored-by: joshua.beckstrand <joshua.beckstrand@utahtech.edu>

--- app.py
+++ app.py
@@ -328,6 +328,18 @@ def create_app():
         ]
         return jsonify({"fact": facts[0]})
 
+    @app.route('/motivation', methods=['GET'])
+    def get_motivation():
+        motivational_quotes = [
+            "The only way to do great work is to love what you do.",
+            "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+            "Believe you can and you're halfway there.",
+            "Act as if what you do makes a difference. It does.",
+            "The harder you work for something, the greater you’ll feel when you achieve it."
+        ]
+        selected_quote = random.choice(motivational_quotes)
+        return jsonify({"motivational_quote": selected_quote})
+ 
     @app.route('/quotes', methods=['GET'])
     def fav_quotes():
         quotes = [
