92c98b26a1ef256aa0f484d3d3260c1b7d29d55c
Andres Pinto Dajer <d00411174@utahtech.edu>
2024-10-18 18:40:20 -0600

    docker

--- endpoints/pizza.py
+++ endpoints/pizza.py
@@ -18,23 +18,30 @@ def pizza():
     ]
     crusts = ["Hand Tossed", "Handmade Pan", "Crunchy Thin Crust"]
     
-    # New Cheese Levels
+    # Cheese Levels
     cheese_levels = ["Light Cheese", "Regular Cheese", "Extra Cheese"]
     
-    # Select random elements
-    selected_sauce = random.choice(sauces)
-    selected_toppings = random.sample(toppings, 3)
-    selected_crust = random.choice(crusts)
-    selected_cheese = request.args.get('cheese', 'Regular Cheese')
-    
-    if selected_cheese not in cheese_levels:
-        return jsonify({"error": "Invalid cheese level"}), 400  
+    # Function to create a single pizza
+    def create_pizza():
+        selected_sauce = random.choice(sauces)
+        selected_toppings = random.sample(toppings, 3)
+        selected_crust = random.choice(crusts)
+        selected_cheese = request.args.get('cheese', 'Regular Cheese')
 
-    pizza = {
-        "crust": selected_crust,
-        "sauce": selected_sauce,
-        "toppings": selected_toppings,
-        "cheese": selected_cheese  
-    }
+        if selected_cheese not in cheese_levels:
+            return {"error": "Invalid cheese level"}, 400
 
-    return jsonify(pizza)
+        return {
+            "crust": selected_crust,
+            "sauce": selected_sauce,
+            "toppings": selected_toppings,
+            "cheese": selected_cheese
+        }
+
+    # RNG to determine if user gets one or two pizzas
+    pizza_count = random.choice([1, 2])
+
+    if pizza_count == 1:
+        return jsonify(create_pizza())
+    else:
+        return jsonify([create_pizza(), create_pizza()])
