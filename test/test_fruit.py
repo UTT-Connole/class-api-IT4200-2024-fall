d51da9fa5ade319b84c32785b569318286977740
Christian Bassilios <cbassilios@gmail.com>
2024-11-03 06:13:02 -0700

    minor changes made to fruitInfo

--- test/test_fruit.py
+++
@@ -1,9 +0,0 @@
-def test_fruit_info(client):
-    response = client.get('/fruitInfo?fruit=apple')
-    assert response.status_code == 200
-    data = response.get_json()
-    assert data == {
-        "fruit": "apple",
-        "color": "red",
-        "taste": "sweet"
-    }
