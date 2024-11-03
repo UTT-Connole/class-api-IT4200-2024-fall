a055a54ad18eeecb0a0d36907ba5f5b5f779359c
Christian Bassilios <cbassilios@gmail.com>
2024-11-03 05:55:51 -0700

    minor changes made to fruitInfo

--- test/test_fruitinfo.py
+++ test/test_fruitinfo.py
@@ -1,12 +1,4 @@
 import json
-
-def test_fruit_info_with_valid_fruit(client):
-    response = client.get('/fruitInfo?fruit=apple')
-    assert response.status_code == 200
-    data = response.get_json()
-    assert data["fruit"] == "apple"
-    assert data["color"] == "red"
-    assert data["taste"] == "sweet"
     
 def test_get_non_existing_fruit(client):
     response = client.get('/fruitInfo?fruit=kiwi')
@@ -29,14 +21,6 @@ def test_post_fruit_incomplete_data(client):
     response = client.post('/fruitInfo?fruit=cherry&color=red')
     data = json.loads(response.data)
     assert 'error' in data
-    
-def test_get_existing_fruit(client):
-    response = client.get('/fruitInfo?fruit=peach')
-    data = json.loads(response.data)
-    assert 'fruit' in data
-    assert data['fruit'] == 'peach'
-    assert data['color'] == 'yellow'
-    assert data['taste'] == 'sweet'
 
 def test_get_all_fruits(client):
     response = client.get('/fruitInfo')
