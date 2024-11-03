62dd1977969d5871478f59370cad6721311fcfc3
Christian Bassilios <89408016+ChristianBass@users.noreply.github.com>
2024-11-03 12:15:02 -0700

    Fruit info minor (#260)
    * minor changes made to fruitInfo
    
    * minor changes made to fruitInfo
    
    * minor changes made to fruitInfo
    
    * minor changes made to fruitInfo
    
    * minor changes made to fruitInfo
    
    * minor changes made to fruitInfo
    
    * minor changes made to fruitInfo
    
    * minor changes made to fruitInfo

--- test/test_fruitinfo.py
+++ test/test_fruitinfo.py
@@ -1,22 +1,23 @@
-def test_fruit_info_with_valid_fruit(client):
-    response = client.get('/fruitInfo?fruit=apple')
-    assert response.status_code == 200
-    data = response.get_json()
-    assert data["fruit"] == "apple"
-    assert data["color"] == "red"
-    assert data["taste"] == "sweet"
+import json
 
-def test_fruit_info_with_invalid_fruit(client):
-    response = client.get('/fruitInfo?fruit=unknownfruit')
-    assert response.status_code == 404
-    data = response.get_json()
-    assert "error" in data
-    assert data["error"] == "Fruit not found. Please try apple, banana, lemon, orange, grape, or lime."
+def test_post_new_fruit(client):
+    response = client.post('/fruitInfo?fruit=plum&color=purple&taste=sweet')
+    data = json.loads(response.data)
+    assert 'message' in data
+    assert data['message'] == 'plum added successfully!'
 
-def test_fruit_info_case_insensitive(client):
-    response = client.get('/fruitInfo?fruit=Banana')
-    assert response.status_code == 200
-    data = response.get_json()
-    assert data["fruit"] == "Banana"
-    assert data["color"] == "yellow"
-    assert data["taste"] == "sweet"
+def test_post_fruit_missing_data(client):
+    response = client.post('/fruitInfo?fruit=&color=yellow&taste=sour')
+    data = json.loads(response.data)
+    assert 'error' in data
+
+def test_post_fruit_incomplete_data(client):
+    response = client.post('/fruitInfo?fruit=cherry&color=red')
+    data = json.loads(response.data)
+    assert 'error' in data
+
+def test_get_all_fruits(client):
+    response = client.get('/fruitInfo')
+    data = json.loads(response.data)
+    assert data
+    assert 'plum' in data
