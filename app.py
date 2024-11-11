b8698af70cf3ff94f204c1132da1b9d71d055031
dalhougaard <159732240+dalhougaard@users.noreply.github.com>
2024-11-10 20:15:39 -0700

    fix(favoritequote): moved favoritequote endpoint from app.py into quo… (#280)
    * fix(favoritequote): moved favoritequote endpoint from app.py into quotes.py endpoint
    
    * fix(favoritequote): fixed indentation
    
    ---------
    
    Co-authored-by: dhougaard <d00415511@dmail.dixie.edu>

--- app.py
+++ app.py
@@ -129,33 +129,6 @@ def create_app():
     def hello_world():
         return render_template('index.html')
         
-    @app.route('/favoritequote', methods=['GET', 'POST', 'PATCH'])
-    def get_favorite_quote():
-        favorite_quote = {
-         "quote": "The only way to do great work is to love what you do.",
-         "author": "Steve Jobs"
-        }
-        quotes = [favorite_quote]
-        if request.method == 'GET':
-            return jsonify(favorite_quote)  
-
-        elif request.method == 'POST':
-            new_quote = request.json 
-            quotes.append(new_quote)  
-            return jsonify({"message": "New favorite quote added!", "quote": new_quote}), 201
-    
-        elif request.method == 'PATCH':
-        # Assuming the client sends the author's name to identify which quote to update
-            author = request.json.get('author')
-            updated_quote = request.json.get('quote')
-        
-        for quote in quotes:
-            if quote['author'] == author:
-                quote['quote'] = updated_quote
-                return jsonify({"message": "Favorite quote updated!", "quote": quote}), 200
-        
-        return jsonify({"error": "Quote not found for the given author."}), 404
-
     @app.route('/fortune', methods=['GET'])
     def get_fortune():
         fortunes = [
