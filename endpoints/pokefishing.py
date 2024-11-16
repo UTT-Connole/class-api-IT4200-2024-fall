905c3ed034e6bc6db3983992fbf15076ff1c8053
dhougaard <d00415511@dmail.dixie.edu>
2024-11-16 16:18:18 -0700

    feat(pokefishing): migrating data to dynamodb wip

--- endpoints/pokefishing.py
+++ endpoints/pokefishing.py
@@ -1,30 +1,30 @@
 from flask import Blueprint, jsonify, request
 import random
+import os, boto3
 
 pokefishing_bp = Blueprint('pokefishing', __name__)
 
-magikarp = [
-    "a regular ol' Magikarp",
-    "a calico pattern Magikarp",
-    "a orange two-tone pattern Magikarp",
-    "a pink dapple pattern Magikarp",
-    "a gray diamond pattern Magikarp",
-    "a purple patches pattern Magikarp",
-    "a apricot tiger pattern Magikarp",
-    "a brown stripes pattern Magikarp",
-    "a orange forehead pattern Magikarp",
-    "a blue raindrops pattern Magikarp",
-    "a shiny Magikarp",
-    "a... Oh no, it's a Gyarados!!",
-    "a Goldeen and it's the biggest you've ever seen",
-    "nothing... But you did see a Mudkip riding on a Lotad"
-]
+# Access DynamoDB table
+dynamo_url = os.environ.get('DYNAMO_URL') or 'http://localhost:8000'
+dynamo_region = os.environ.get('DYNAMO_REGION') or 'us-west-2'
+print('dynamo_url:', dynamo_url)
+print('dynamo_region:', dynamo_region)
+dynamodb = boto3.resource('dynamodb', endpoint_url=dynamo_url, region_name=dynamo_region)
+table = dynamodb.Table('pokefishing')
 
 @pokefishing_bp.route('/pokefishing', methods=['GET'])
 def fish():
     success = random.choice([True, False])
     if success:
-        caught = random.choice(magikarp)
+        caught = table.scan()
     else:
         caught = "... Oops, you forgot to reel it in"
     return jsonify({"You caught": caught + "!"})
+
+    try:
+        response = table.scan()
+        return jsonify(response['Items']), 200
+
+    except Exception as e:
+        print(f"Error accessing DynamoDB: {str(e)}")
+        return jsonify({"error": "Failed to access DynamoDB", "details": str(e)}), 500
